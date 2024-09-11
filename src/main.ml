04a3d3c3a2b2c2184c407fbc258f407700cf10ba
Nanapiou <nana.juniot@gmail.com>
2024-09-11 09:43:22 +0200

    To continue

--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,4 @@
-open Infer
-open TypUtil
+(* open Infer *)
+(* open TypUtil *)
 
-let () = print_typ @@ infer "let f = fun x -> x in if f true then 0 else f 666"
+(* let () = print_typ @@ infer "let f = fun x -> x in if f true then 0 else f 666" *)
