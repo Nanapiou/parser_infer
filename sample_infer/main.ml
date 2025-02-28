0e5a3c38e73d1865c7be696874f6a57087d05917
nanapiou <nana.juniot@gmail.com>
2025-02-28 15:45:05 +0100

    Just repopened it

--- sample_infer/main.ml
+++ sample_infer/main.ml
@@ -1,11 +1,11 @@
 open Infer
 
-let file = open_in "./patho.txt"
+let file = open_in "./sample.txt"
 let text = In_channel.input_all file
 
 (* let () = Util.print_expr @@ Pase.parse text; print_newline () *)
 let () = try
-    (* Util.print_typ *) ignore @@ infer text
+    Util.print_typ @@ infer text
   with NoUnifier (t1, t2) ->
     print_endline "No unifier:";
     Util.print_typ t1; print_string " <> "; Util.print_typ t2
